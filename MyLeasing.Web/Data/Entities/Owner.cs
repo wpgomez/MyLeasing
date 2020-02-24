222f3ea4d35bdb36b055b54a4c5fce885e59a6fd
Willy Gomez <wp_gomez@hotmail.com>
2020-02-24 17:11:46 -0500

    User Identities

--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -6,36 +6,8 @@ namespace MyLeasing.Web.Data.Entities
     public class Owner
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "The field {0} es mandatory.")]
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        public string Document { get; set; }
-
-        [Required(ErrorMessage = "The field {0} es mandatory.")]
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "First Name")]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "The field {0} es mandatory.")]
-        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "Fixed Phone")]
-        public string FixedPhone { get; set; }
-
-        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
-        [Display(Name = "Cell Phone")]
-        public string CellPhone { get; set; }
-
-        public string Address { get; set; }
-
-        [Display(Name = "Owner Name")]
-        public string FullName => $"{FirstName} {LastName}";
-
-        [Display(Name = "Owner Name")]
-        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+                
+        public User User { get; set; }
 
         public ICollection<Property> Properties { get; set; }
 
