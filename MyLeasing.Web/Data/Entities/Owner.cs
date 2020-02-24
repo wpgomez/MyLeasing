4b4d9331e2e9d49f80d72a98b5e5a6d5faca4fcb
Willy Gomez <wp_gomez@hotmail.com>
2020-02-24 11:56:41 -0500

    Create DB

---
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Document { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}
